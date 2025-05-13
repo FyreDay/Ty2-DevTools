7e6fd3426d8fc4b858cfaedd67b69a373048e23d
FyreDay <fyredaymc@gmail.com>
2025-05-13 02:17:18 -0500

    add more functions

--- Ty2-DevTools/GetMKObjectWindow.cpp
+++ Ty2-DevTools/GetMKObjectWindow.cpp
@@ -29,6 +29,32 @@ void GetMKObjectWindow::Draw(int outerWidth, int outerHeight, float uiScale) {
         }
     }
     ImGui::InputText("Pointer", message.data(), message.size(), ImGuiInputTextFlags_ReadOnly);
+    ImGui::InputText("Mission Ptr", mptr, IM_ARRAYSIZE(id));
+    if (ImGui::Button("Add Mission")) {
+        try {
+            // Try converting the char array to an int using std::stoi
+            uintptr_t addr = static_cast<uintptr_t>(std::stoull(mptr, nullptr, 16));
+            bool success = InsertMission(addr);
+            SetMessage(success ? "Wow!": "Awww");
+        }
+        catch (const std::invalid_argument& e) {
+            SetMessage("uh oh");
+            API::LogPluginMessage(e.what());
+        }
+    }
+    ImGui::InputText("Opal Amount", opalcount, IM_ARRAYSIZE(id));
+    if (ImGui::Button("Add Opals")) {
+        try {
+            // Try converting the char array to an int using std::stoi
+            int value = std::atoi(opalcount);
+            AddOpals(value);
+            SetMessage("Tried to add Opals");
+        }
+        catch (const std::invalid_argument& e) {
+            SetMessage("uh oh");
+            API::LogPluginMessage(e.what());
+        }
+    }
     ImGui::End();
 }
 
@@ -46,4 +72,22 @@ uintptr_t GetMKObjectWindow::FindObjectPtr(int objectId) {
         mov result, eax
     }
     return result;
+}
+
+bool GetMKObjectWindow::InsertMission(uintptr_t missionPtr) {
+    using insertmission = uint32_t(__thiscall*)(uintptr_t thisPtr, uintptr_t objPtr);
+    insertmission getObjPtr = reinterpret_cast<insertmission>((uintptr_t)API::Get()->param()->TyHModule + 0x00117670);
+    uintptr_t thisPtr = (uintptr_t)API::Get()->param()->TyHModule + 0x4EB580 + 0x1bc;
+
+    bool success = (getObjPtr(thisPtr, missionPtr) & 0xFF) != 0;
+    return success;
+}
+
+void GetMKObjectWindow::AddOpals(int amount) {
+    using Addopals = uint32_t(__thiscall*)(uintptr_t thisPtr, uintptr_t objPtr);
+    Addopals addopals = reinterpret_cast<Addopals>((uintptr_t)API::Get()->param()->TyHModule + 0x00119fb0);
+    uintptr_t thisPtr = (uintptr_t)API::Get()->param()->TyHModule+ 0x4EB580;
+
+    addopals(thisPtr, amount);
+    return;
 }
--- Ty2-DevTools/GetMKObjectWindow.h
+++ Ty2-DevTools/GetMKObjectWindow.h
@@ -12,7 +12,11 @@ public:
 	void Draw(int outerWidth, int outerHeight, float uiScale) override;
 	void SetMessage(std::string);
 	uintptr_t FindObjectPtr(int objectId);
+	bool InsertMission(uintptr_t missionPtr);
+	void AddOpals(int amount);
 	char id[128] = "";
+	char mptr[128] = "";
+	char opalcount[128] = "";
 private:
 	std::string message;
 };
